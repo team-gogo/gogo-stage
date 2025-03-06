c83f9420ece2b6fdced6b3511351280413c85a08
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2025-03-06 16:41:31 +0900

    update: RequestMapping url /community -> /stage/community

--- src/main/kotlin/gogo/gogostage/domain/community/root/presentation/CommunityController.kt
+++ src/main/kotlin/gogo/gogostage/domain/community/root/presentation/CommunityController.kt
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/community")
+@RequestMapping("/stage/community")
 class CommunityController(
     private val communityService: CommunityService
 ) {
