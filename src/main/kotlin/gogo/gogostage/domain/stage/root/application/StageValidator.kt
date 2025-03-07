ae71b5baea83f5bfb3aab699a9bbcd6b66514c97
신희성 <vvwv3092@gmail.com>
2025-03-07 11:59:18 +0900

    update: stage validator

--- src/main/kotlin/gogo/gogostage/domain/stage/root/application/StageValidator.kt
+++ src/main/kotlin/gogo/gogostage/domain/stage/root/application/StageValidator.kt
@@ -29,29 +29,29 @@ class StageValidator {
     }
 
     private fun validShop(dto: CreateOfficialStageDto) {
-        if (dto.shop.coinToss.isActive && dto.shop.coinToss.price == null || dto.shop.coinToss.quantity == null) {
+        if (dto.shop.coinToss.isActive && (dto.shop.coinToss.price == null || dto.shop.coinToss.quantity == null)) {
             throw StageException("CoinToss 미니게임의 티켓 가격, 수량을 입력하세요.", HttpStatus.BAD_REQUEST.value())
         }
 
-        if (dto.shop.yavarwee.isActive && dto.shop.yavarwee.price == null || dto.shop.yavarwee.quantity == null) {
+        if (dto.shop.yavarwee.isActive && (dto.shop.yavarwee.price == null || dto.shop.yavarwee.quantity == null)) {
             throw StageException("Yavarwee 미니게임의 티켓 가격, 수량을 입력하세요.", HttpStatus.BAD_REQUEST.value())
         }
 
-        if (dto.shop.plinko.isActive && dto.shop.plinko.price == null || dto.shop.plinko.quantity == null) {
+        if (dto.shop.plinko.isActive && (dto.shop.plinko.price == null || dto.shop.plinko.quantity == null)) {
             throw StageException("Plinko 미니게임의 티켓 가격, 수량을 입력하세요.", HttpStatus.BAD_REQUEST.value())
         }
     }
 
     private fun validMiniGame(dto: CreateOfficialStageDto) {
-        if (dto.miniGame.coinToss.isActive && dto.miniGame.coinToss.maxBettingPoint == null) {
+        if (dto.miniGame.coinToss.isActive && (dto.miniGame.coinToss.maxBettingPoint == null)) {
             throw StageException("CoinToss 미니게임의 최대 배팅 포인트를 입력하세요.", HttpStatus.BAD_REQUEST.value())
         }
 
-        if (dto.miniGame.yavarwee.isActive && dto.miniGame.yavarwee.maxBettingPoint == null) {
+        if (dto.miniGame.yavarwee.isActive && (dto.miniGame.yavarwee.maxBettingPoint == null)) {
             throw StageException("Yavarwee 미니게임의 최대 배팅 포인트를 입력하세요.", HttpStatus.BAD_REQUEST.value())
         }
 
-        if (dto.miniGame.plinko.isActive && dto.miniGame.plinko.maxBettingPoint == null) {
+        if (dto.miniGame.plinko.isActive && (dto.miniGame.plinko.maxBettingPoint == null)) {
             throw StageException("Plinko 미니게임의 최대 배팅 포인트를 입력하세요.", HttpStatus.BAD_REQUEST.value())
         }
     }
