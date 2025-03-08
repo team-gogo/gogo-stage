96792f420f3c6c1e411d067bc2f6d8ab189b491c
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2025-03-09 03:42:30 +0900

    add: StudentByIdsStub data class

--- src/main/kotlin/gogo/gogostage/global/internal/student/stub/StudentStub.kt
+++ src/main/kotlin/gogo/gogostage/global/internal/student/stub/StudentStub.kt
@@ -16,3 +16,13 @@ data class StudentByIdStub(
     val isActiveProfanityFilter: Boolean,
     val createdAt: LocalDateTime
 )
+
+data class StudentByIdsStub(
+    val studentId: Long,
+    val schoolId: Long,
+    val name: String,
+    val deviceToken: String?,
+    val sex: Sex,
+    val classNumber: Int,
+    val studentNumber: Int
+)
