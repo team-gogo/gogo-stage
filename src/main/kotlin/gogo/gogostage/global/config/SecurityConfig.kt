2720a64759638460db89784a5e111f147e5c6981
신희성 <vvwv3092@gmail.com>
2025-03-11 16:05:41 +0900

    add: team apply api

--- src/main/kotlin/gogo/gogostage/global/config/SecurityConfig.kt
+++ src/main/kotlin/gogo/gogostage/global/config/SecurityConfig.kt
@@ -50,6 +50,7 @@ class SecurityConfig(
             httpRequests.requestMatchers(HttpMethod.POST, "/stage/fast").hasAnyRole(Authority.USER.name, Authority.STAFF.name)
             httpRequests.requestMatchers(HttpMethod.POST, "/stage/official").hasAnyRole(Authority.USER.name, Authority.STAFF.name)
             httpRequests.requestMatchers(HttpMethod.POST, "/stage/join/{stage_id}").hasAnyRole(Authority.USER.name, Authority.STAFF.name)
+            httpRequests.requestMatchers(HttpMethod.POST, "/stage/team/{game_id}").hasAnyRole(Authority.USER.name, Authority.STAFF.name)
 
             // server to server
             httpRequests.requestMatchers(HttpMethod.GET, "/stage/api/point/{stage_id}").permitAll()
