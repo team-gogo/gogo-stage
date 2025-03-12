0d5461ae43670e4554710a4ccad4af8f55f5083c
신희성 <vvwv3092@gmail.com>
2025-03-12 20:07:40 +0900

    add: stage confirm api

--- src/main/kotlin/gogo/gogostage/domain/stage/root/presentation/StageController.kt
+++ src/main/kotlin/gogo/gogostage/domain/stage/root/presentation/StageController.kt
@@ -3,6 +3,7 @@ package gogo.gogostage.domain.stage.root.presentation
 import gogo.gogostage.domain.stage.root.application.StageService
 import gogo.gogostage.domain.stage.root.application.dto.CreateFastStageDto
 import gogo.gogostage.domain.stage.root.application.dto.CreateOfficialStageDto
+import gogo.gogostage.domain.stage.root.application.dto.StageConfirmDto
 import gogo.gogostage.domain.stage.root.application.dto.StageJoinDto
 import jakarta.validation.Valid
 import org.springframework.http.HttpStatus
@@ -37,6 +38,15 @@ class StageController(
         @RequestBody dto: StageJoinDto,
     ): ResponseEntity<Unit> {
         stageService.join(stageId, dto)
+        return ResponseEntity.ok().build()
+    }
+
+    @PostMapping("/confirm/{stage_id}")
+    fun confirm(
+        @PathVariable("stage_id") stageId: Long,
+        @RequestBody dto: StageConfirmDto,
+    ): ResponseEntity<Unit> {
+        stageService.confirm(stageId, dto)
         return ResponseEntity.ok().build()
     }
 
--- src/main/kotlin/gogo/gogostage/global/config/SecurityConfig.kt
+++ src/main/kotlin/gogo/gogostage/global/config/SecurityConfig.kt
@@ -51,6 +51,7 @@ class SecurityConfig(
             httpRequests.requestMatchers(HttpMethod.POST, "/stage/official").hasAnyRole(Authority.USER.name, Authority.STAFF.name)
             httpRequests.requestMatchers(HttpMethod.POST, "/stage/join/{stage_id}").hasAnyRole(Authority.USER.name, Authority.STAFF.name)
             httpRequests.requestMatchers(HttpMethod.POST, "/stage/team/{game_id}").hasAnyRole(Authority.USER.name, Authority.STAFF.name)
+            httpRequests.requestMatchers(HttpMethod.POST, "/stage/confirm/{stage_id}").hasAnyRole(Authority.USER.name, Authority.STAFF.name)
 
             // server to server
             httpRequests.requestMatchers(HttpMethod.GET, "/stage/api/point/{stage_id}").permitAll()
