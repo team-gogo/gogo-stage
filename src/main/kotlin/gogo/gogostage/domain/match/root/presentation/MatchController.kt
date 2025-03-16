9553143ac0b1f7e1be7b479ccb056afed4eaa113
신희성 <vvwv3092@gmail.com>
2025-03-16 16:09:13 +0900

    add: match search api

--- src/main/kotlin/gogo/gogostage/domain/match/root/presentation/MatchController.kt
+++ src/main/kotlin/gogo/gogostage/domain/match/root/presentation/MatchController.kt
@@ -2,11 +2,11 @@ package gogo.gogostage.domain.match.root.presentation
 
 import gogo.gogostage.domain.match.root.application.MatchService
 import gogo.gogostage.domain.match.root.application.dto.MatchApiInfoDto
+import gogo.gogostage.domain.match.root.application.dto.MatchSearchDto
+import jakarta.validation.Valid
+import jakarta.validation.constraints.NotNull
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/stage")
@@ -19,6 +19,17 @@ class MatchController(
         @RequestParam matchId: Long
     ): ResponseEntity<MatchApiInfoDto> {
         val response = matchService.matchApiInfo(matchId)
+        return ResponseEntity.ok(response)
+    }
+
+    @GetMapping("/match/search/{stage_id}")
+    fun search(
+        @PathVariable("stage_id") stageId: Long,
+        @RequestParam @Valid @NotNull y: Int,
+        @RequestParam @Valid @NotNull m: Int,
+        @RequestParam @Valid @NotNull d: Int,
+    ): ResponseEntity<MatchSearchDto> {
+        val response = matchService.search(stageId, y, m, d)
         return ResponseEntity.ok(response)
     }
 
--- src/main/kotlin/gogo/gogostage/global/config/SecurityConfig.kt
+++ src/main/kotlin/gogo/gogostage/global/config/SecurityConfig.kt
@@ -52,7 +52,8 @@ class SecurityConfig(
             httpRequests.requestMatchers(HttpMethod.POST, "/stage/join/{stage_id}").hasAnyRole(Authority.USER.name, Authority.STAFF.name)
             httpRequests.requestMatchers(HttpMethod.POST, "/stage/team/{game_id}").hasAnyRole(Authority.USER.name, Authority.STAFF.name)
             httpRequests.requestMatchers(HttpMethod.POST, "/stage/confirm/{stage_id}").hasAnyRole(Authority.USER.name, Authority.STAFF.name)
-            
+            httpRequests.requestMatchers(HttpMethod.POST, "/stage/match/search/{stage_id}").hasAnyRole(Authority.USER.name, Authority.STAFF.name)
+
             // community
             httpRequests.requestMatchers(HttpMethod.POST, "/stage/community/{game_id}").hasAnyRole(Authority.USER.name, Authority.STAFF.name)
             httpRequests.requestMatchers(HttpMethod.GET, "/stage/community/{stage_id}").hasAnyRole(Authority.USER.name, Authority.STAFF.name)
