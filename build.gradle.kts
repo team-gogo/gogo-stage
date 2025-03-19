93eb824f9a5798806219a225b5d7b7ee3895163f
신희성 <vvwv3092@gmail.com>
2025-03-19 12:21:07 +0900

    add: logging filter

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,8 @@ repositories {
 extra["springCloudVersion"] = "2024.0.0-RC1"
 
 dependencies {
+    runtimeOnly("io.micrometer:micrometer-registry-prometheus")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.kafka:spring-kafka")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-security")
