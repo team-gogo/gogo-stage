129f8b3f765b8d7d30e6377f61bd3e4cfe246032
신희성 <vvwv3092@gmail.com>
2025-03-26 09:25:06 +0900

    update: match search dto - is player, is notice field

--- src/main/kotlin/gogo/gogostage/domain/match/root/application/dto/MatchDto.kt
+++ src/main/kotlin/gogo/gogostage/domain/match/root/application/dto/MatchDto.kt
@@ -51,6 +51,8 @@ data class MatchSearchInfoDto(
     val gameName: String,
     val system: GameSystem,
     val turn: Int?,
+    val isNotice: Boolean,
+    val isPlayer: Boolean,
     val betting: MatchBettingInfoDto?,
     val result: MatchResultInfoDto?
 )
