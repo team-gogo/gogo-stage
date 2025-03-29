d11c257b89767b88c77b23cda149661cc90c5b94
신희성 <vvwv3092@gmail.com>
2025-03-30 04:28:44 +0900

    update: batch cancel match result delete queyr

--- src/main/kotlin/gogo/gogostage/domain/match/result/persistence/MatchResultRepository.kt
+++ src/main/kotlin/gogo/gogostage/domain/match/result/persistence/MatchResultRepository.kt
@@ -1,7 +1,11 @@
 package gogo.gogostage.domain.match.result.persistence
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Modifying
+import org.springframework.data.jpa.repository.Query
 
 interface MatchResultRepository: JpaRepository<MatchResult, Long> {
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    @Query("DELETE FROM MatchResult mr WHERE mr.match.id = :matchId")
     fun deleteByMatchId(matchId: Long)
 }
