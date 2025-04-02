17dba479c26c0dd5011b445de7633daa10d74079
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2025-04-02 11:25:16 +0900

    fix: solving concurrency issue

--- src/main/kotlin/gogo/gogostage/domain/match/notification/persistence/MatchNotificationRepository.kt
+++ src/main/kotlin/gogo/gogostage/domain/match/notification/persistence/MatchNotificationRepository.kt
@@ -1,9 +1,12 @@
 package gogo.gogostage.domain.match.notification.persistence
 
+import jakarta.persistence.LockModeType
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Lock
 import org.springframework.data.jpa.repository.Query
 
 interface MatchNotificationRepository: JpaRepository<MatchNotification, Long> {
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("SELECT mn FROM MatchNotification mn WHERE mn.match.id = :matchId AND mn.studentId = :studentId")
     fun findByMatchIdAndStudentIdForWrite(matchId: Long, studentId: Long): MatchNotification?
     fun existsByMatchIdAndStudentId(matchId: Long, studentId: Long): Boolean
