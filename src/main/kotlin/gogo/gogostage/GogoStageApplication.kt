2f9446fe0ed386a3414a9224d476205cb8d73cb9
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2025-04-28 09:21:41 +0900

    delete: EnableAsync Annotation

--- src/main/kotlin/gogo/gogostage/GogoStageApplication.kt
+++ src/main/kotlin/gogo/gogostage/GogoStageApplication.kt
@@ -5,7 +5,6 @@ import org.springframework.boot.runApplication
 import org.springframework.scheduling.annotation.EnableAsync
 import org.springframework.scheduling.annotation.EnableScheduling
 
-@EnableAsync
 @EnableScheduling
 @SpringBootApplication
 class GogoStageApplication
